2e767accd4f207d4e9079af2227d0f46e5f66c71
Baiel <apple@Baiel.local>
2024-12-01 00:53:50 +0600

    the last one

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,15 +27,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 
 
 ALLOWED_HOSTS = ['*']
 
 
-# CSRF_TRUSTED_ORIGINS = [
-#     'https://django-server-0lh5-production.up.railway.app',#django-server-0lh5-production.up.railway.app
-# ]
+CSRF_TRUSTED_ORIGINS = [
+    'https://django-server-0lh5-production.up.railway.app',
+]
+
 
 
 # Application definition
