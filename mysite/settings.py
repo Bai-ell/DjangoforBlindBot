39303f1e0c2c32e2e19097bef0e5cd1b012c4850
Baiel <apple@Baiel.local>
2024-12-01 00:43:21 +0600

    change usrl

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 DEBUG = False
 
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    'django-server-0lh5-production.up.railway.app',]
 
 
 CSRF_TRUSTED_ORIGINS = [
