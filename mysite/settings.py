27c891b609ff4c92c7469a9c8a7b795ab3b38d5c
Baiel <apple@Baiel.local>
2024-11-30 23:28:49 +0600

    settings change

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,7 +33,10 @@ ALLOWED_HOSTS = ["*"]
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
-# CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    'https://django-server-0lh5-production.up.railway.app',
+]
+
 
 # Application definition
 
