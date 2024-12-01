adab7b1169e2b98537b0e7827558f7254bf7593f
Baiel <apple@Baiel.local>
2024-12-02 00:36:45 +0600

    static urls

--- mysite/settings.py
+++ mysite/settings.py
@@ -105,28 +105,28 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.getenv("PGDATABASE"),     
-#         'USER': os.getenv("PGUSER"),        
-#         'PASSWORD': os.getenv("PGPASSWORD"),  
-#         'HOST': os.getenv("PGHOST"),         
-#         'PORT': os.getenv("PGPORT"),          
-#     }
-# }
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'pjzxlXJZnQbgJzuMvAtwloBedZyPInod',
-        'HOST': 'postgres-c_7e.railway.internal',
-        'PORT': '5432',
+        'NAME': os.getenv("PGDATABASE"),     
+        'USER': os.getenv("PGUSER"),        
+        'PASSWORD': os.getenv("PGPASSWORD"),  
+        'HOST': os.getenv("PGHOST"),         
+        'PORT': os.getenv("PGPORT"),          
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'pjzxlXJZnQbgJzuMvAtwloBedZyPInod',
+#         'HOST': 'postgres-c_7e.railway.internal',
+#         'PORT': '5432',
+#     }
+# }
 
 
 
