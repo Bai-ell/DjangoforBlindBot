28aeb167b9ff55190db74ebfb90b157fef68c9ad
Baiel <apple@Baiel.local>
2024-12-02 00:11:02 +0600

    change setting for user

--- manage.py
+++ manage.py
@@ -2,44 +2,19 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import django
-
-def create_superuser():
-    """Создание суперпользователя"""
-    # Импортируем модели только после настройки Django
-    from django.contrib.auth.models import User
-
-    username = "admin5"
-    email = "admin5@example.com"
-    password = "5"
-
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, email=email, password=password)
-        print("Суперпользователь создан")
-    else:
-        print("Суперпользователь уже существует")
 
 
 def main():
     """Run administrative tasks."""
-    # Устанавливаем DJANGO_SETTINGS_MODULE перед импортом Django
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
     try:
-        # Настраиваем Django перед использованием его API
-        django.setup()
+        from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-
-    # После настройки Django вызываем функцию создания суперпользователя
-    create_superuser()
-
-    # Выполняем стандартные команды Django
-    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
 
